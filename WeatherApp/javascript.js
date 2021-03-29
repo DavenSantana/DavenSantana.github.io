761c6d79cfbf4923e2fa104a02dfb90d3a9cfbec
Daven <daven.santana38@qmail.cuny.edu>
2021-03-28 21:31:31 -0400

    Finalizing Weather app API

--- WeatherApp/javascript.js
+++ WeatherApp/javascript.js
@@ -25,13 +25,26 @@ $(document).ready(function () {
     // Update Dom
     function updateDOM(data) {
         var city = data.name;
-        var temp = Math.round(data.main.temp_max);
+        var temp = Math.round(data.main.temp);
+        temp = Math.round(CtoF(temp));
         var desc = data.weather[0].description;
         var icon = data.weather[0].icon;
+        var humidity = data.main.humidity;
+        var high = Math.round(CtoF(data.main.temp_max));
+        var low = Math.round(CtoF(data.main.temp_min));
+        var wind = data.wind.speed;
 
         $('#city').html(city);
         $('#temp').html(temp);
         $('#desc').html(desc);
         $('#icon').attr('src', icon);
+        $("#humidity").append(" " + humidity);
+        $("#high").append(" " + high);
+        $("#low").append(" " + low);
+        $("#wind").append(" " + wind);
     }
 });
+
+function CtoF(temp){
+    return (temp * 9/5) + 32;
+}
